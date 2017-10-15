23f824495f6d4f82eb2bd50649cfeabc77d3151a
Xiao_x1998 <1368994233@qq.com>
2017-10-15 13:43:07 +0800

    add lab2

--- lab2/Lab2/app/src/main/java/com/example/user/lab1/MainActivity.java
+++ lab2/Lab2/app/src/main/java/com/example/user/lab1/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.user.lab1;
 
 import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.Toast;
+
+import java.io.File;
 
 import static com.example.user.lab1.R.layout.activity_main;
 
@@ -19,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
     EditText edit_student_id,edit_password;
     public RadioButton rt1,rt2;
     public int flag;
+
+    public final static int ALBUM_REQUEST_CODE = 1;
+    public final static int CAMERA_REQUEST_CODE = 2;
+    public static String SAVED_IMAGE_DIR_PATH = Environment.getExternalStorageDirectory().getPath() + "/Pictures/";
+    String cameraPath;
+    private Context activity;
+    public ImageButton imagebutton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +49,9 @@ public class MainActivity extends AppCompatActivity {
         edit_student_id = (EditText) findViewById(R.id.edit_student_id);
         edit_password = (EditText) findViewById(R.id.edit_password);
 
+        activity = this.getApplicationContext();
+        imagebutton = (ImageButton) findViewById(R.id.img_sysu);
+
         findViewById(R.id.img_sysu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,6 +61,9 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         Toast.makeText(MainActivity.this,String.format("您选择了[%s]",array[which]), Toast.LENGTH_SHORT).show();
+                        if(array[which].equals("拍摄")) StartCamera();
+                        if(array[which].equals("从相册选择")) ShowPhoto();
+
                     }
                 }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
                     @Override
@@ -57,7 +83,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(rt1.isChecked() && flag != 1)
                 {
-                    Snackbar.make(findViewById(R.id.main_layout),"您选择了学生",Snackbar.LENGTH_SHORT).setAction("确定", new View.OnClickListener() {
+                    Snackbar.make(findViewById(R.id.main_layout),"您选择了学生",Snackbar.LENGTH_SHORT)
+                            .setAction("确定", new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
                             Toast.makeText(MainActivity.this,"Snackbar 的确定按钮被点击了",Toast.LENGTH_SHORT).show();
@@ -73,7 +100,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(rt2.isChecked() && flag != -1)
                 {
-                    Snackbar.make(findViewById(R.id.main_layout),"您选择了教职工",Snackbar.LENGTH_SHORT).setAction("确定", new View.OnClickListener() {
+                    Snackbar.make(findViewById(R.id.main_layout),"您选择了教职工",Snackbar.LENGTH_SHORT)
+                            .setAction("确定", new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
                             Toast.makeText(MainActivity.this,"Snackbar 的确定按钮被点击了",Toast.LENGTH_SHORT).show();
@@ -117,15 +145,17 @@ public class MainActivity extends AppCompatActivity {
                 {
                     textInputLayout_id.setErrorEnabled(false);
                     textInputLayout_password.setErrorEnabled(false);
-                    if(edit_student_id.getText().toString() == "123456" && edit_password.getText().toString() == "6666")
-                        Snackbar.make(findViewById(R.id.main_layout),"登陆成功",Snackbar.LENGTH_SHORT).setAction("确定", new View.OnClickListener() {
+                    if(edit_student_id.getText().toString().equals("123456") && edit_password.getText().toString().equals("6666"))
+                        Snackbar.make(findViewById(R.id.main_layout),"登陆成功",Snackbar.LENGTH_SHORT)
+                                .setAction("确定", new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
                                 Toast.makeText(MainActivity.this,"Snackbar 的确定按钮被点击了",Toast.LENGTH_SHORT).show();
                             }
                         }).show();
                     else
-                        Snackbar.make(findViewById(R.id.main_layout),"学号或密码错误",Snackbar.LENGTH_SHORT).setAction("确定", new View.OnClickListener() {
+                        Snackbar.make(findViewById(R.id.main_layout),"学号或密码错误",Snackbar.LENGTH_SHORT)
+                                .setAction("确定", new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
                                 Toast.makeText(MainActivity.this,"Snackbar 的确定按钮被点击了",Toast.LENGTH_SHORT).show();
@@ -138,11 +168,88 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_register).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(flag == 1) Toast.makeText(MainActivity.this,"学生注册功能尚未启用",Toast.LENGTH_SHORT).show();
+                if(flag == 1) Snackbar.make(findViewById(R.id.main_layout),"学生注册功能尚未启用",Snackbar.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this,"学生注册功能尚未启用",Toast.LENGTH_SHORT).show();
                 else Toast.makeText(MainActivity.this,"教职工注册功能尚未启用",Toast.LENGTH_SHORT).show();
             }
         });
 
 
+    }
+
+    private void StartCamera()
+    {
+        String state = Environment.getExternalStorageState();
+        if(state.equals(Environment.MEDIA_MOUNTED))
+        {
+            cameraPath = SAVED_IMAGE_DIR_PATH + System.currentTimeMillis() + ".jpg";
+            Intent intent = new Intent();
+            intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+            File outDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+//            if(!outDir.exists()) outDir.mkdirs();
+            File outFile = new File(outDir,System.currentTimeMillis()+".jpg");
+            Uri uri = Uri.fromFile(outFile);
+
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,uri);
+            intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 0);
+            startActivityForResult(intent,CAMERA_REQUEST_CODE);
+        }
+    }
+
+    private void ShowPhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK, null);
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, ALBUM_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ALBUM_REQUEST_CODE) {
+            try {
+                Uri uri = data.getData();
+                final String absolutePath = getAbsolutePath(activity, uri);
+                Bitmap bitmap = BitmapFactory.decodeFile(absolutePath);
+                imagebutton.setImageBitmap(bitmap);
+                imagebutton.setImageURI(uri);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            try {
+                Bitmap bitmap = BitmapFactory.decodeFile(cameraPath);
+                imagebutton.setImageBitmap(bitmap);
+                Uri uri = Uri.fromFile(new File(cameraPath));
+                imagebutton.setImageURI(uri);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getAbsolutePath(final Context context, final Uri uri) {
+        if (null == uri) return null;
+        final String scheme = uri.getScheme();
+        String data_new = null;
+        if (scheme == null)
+            data_new = uri.getPath();
+        else if (ContentResolver.SCHEME_FILE.equals(scheme)) {
+            data_new = uri.getPath();
+        } else if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
+            Cursor cursor = context.getContentResolver().query(uri,
+                    new String[]{MediaStore.Images.ImageColumns.DATA}, null, null, null);
+            if (null != cursor) {
+                if (cursor.moveToFirst()) {
+                    int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                    if (index > -1) {
+                        data_new = cursor.getString(index);
+                    }
+                }
+                cursor.close();
+            }
+        }
+        return data_new;
     }
 }
